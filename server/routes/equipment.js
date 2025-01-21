b065fb84a86e779cd3328b6a83a9b05d62b6ddf9
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-21 22:28:51 +0700

    feat: Add and update equipment routes with authentication and role checks

---
+++ server/routes/equipment.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { create, listEquipment, getEquipmentByCategory, updateEquipment, readEquipment,   } = require('../controllers/equipment');
+const { authCheck, roleCheck } = require('../middleware/authCheck');
+const router = express.Router()
+
+//อุปกรณ์สำนักงาน
+router.post('/equipment',authCheck,roleCheck(['ADMIN']),create)
+
+router.get('/equipment',authCheck,listEquipment)
+
+router.get('/equipment/:id',authCheck,readEquipment)
+router.get('/equipment/category/:id',authCheck,getEquipmentByCategory)
+
+router.put('/equipment/:id',authCheck,roleCheck(['ADMIN']),updateEquipment)
+
+module.exports = router
