ee568ccf343d62813072ee58cb5d1a6c8093a298
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-21 13:00:54 +0700

    refactor: add validate image for register functional

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,6 +13,7 @@ exports.register = async (req, res) => {
       userName,
       password,
       role,
+      picture,
     } = req.body;
 
     const user = await prisma.user.findFirst({
@@ -39,6 +40,20 @@ exports.register = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
 
+    // ตรวจสอบว่ามี picture หรือไม่
+    let imageId = null;
+    if (picture) {
+      const image = await prisma.image.create({
+        data: {
+          asset_id: picture.asset_id,
+          public_id: picture.public_id,
+          url: picture.url,
+          secure_url: picture.secure_url,
+        },
+      });
+      imageId = image.id; // เก็บ imageId หากมีรูปภาพ
+    }
+
     const authUser = await prisma.user.create({
       data: {
         firstName: firstName,
@@ -49,6 +64,7 @@ exports.register = async (req, res) => {
         userName: userName,
         password: hashPassword,
         role: role,
+        pictureId: imageId,
       },
     });
 
@@ -65,70 +81,74 @@ exports.login = async (req, res) => {
   try {
     const { userName, password } = req.body;
 
-    const user = await prisma.user.findFirst({
+    const user = await prisma.user.findUnique({
       where: {
         userName: userName,
       },
     });
-    console.log(user.firstName)
+
     if (!user || !user.enabled) {
       return res.status(400).json({
         message: "User Not Found or not Enabled",
       });
     }
 
-    const isMatch = await bcrypt.compare(password,user.password)
-    if(!isMatch){
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(400).json({
         message: "Password Invalid",
       });
     }
 
     const payload = {
-      id:user.id,
-      name:user.firstName,
-      username:user.userName,
-      role: user.role
-    }
-    
-    jwt.sign(payload,process.env.SECRET,{expiresIn: '7d'},(error,token) => {
-      if(error){
-          return res.status(500).json({
-              message: "Server Error"
-          })
-      }
-      res.json({ 
-        payload, 
-        token ,
-        message: "Login Successfully"
-       })
-  })
+      id: user.id,
+      name: user.firstName,
+      username: user.userName,
+      role: user.role,
+    };
 
+    jwt.sign(
+      payload,
+      process.env.SECRET,
+      { expiresIn: "7d" },
+      (error, token) => {
+        if (error) {
+          return res.status(500).json({
+            message: "Server Error",
+          });
+        }
+        res.json({
+          payload,
+          token,
+          message: "Login Successfully",
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
   }
 };
 
-exports.currentUser = async(req,res)=> {
+exports.currentUser = async (req, res) => {
   try {
-    const { userName } = req.body
-    console.log(userName)
-      const user = await prisma.user.findFirst({
-          where:{
-              userName: userName
-          },
-          select:{
-              id:true,
-              email:true,
-              firstName: true,
-              role: true
-          }
-      })
-      res.json({user})
+    const { userName } = req.body;
+    console.log(userName);
+    const user = await prisma.user.findUnique({
+      where: {
+        userName: userName,
+      },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        role: true,
+      },
+    });
+    res.json({ user });
   } catch (error) {
-      console.log(error)
-      res.status(500).json({
-          message: "Server Error"
-      })
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
   }
-}
+};
