1836b5b0db657c4add099bf21f422444d7646c7e
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-21 23:00:54 +0700

    feat: Add routes for creating and retrieving repair reports, including history by user and technician

---
+++ server/routes/reportRepair.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { authCheck } = require('../middleware/authCheck')
+const { create, listReportRepair, readReportRepair, listReportRepairByUser, listReportRepairByTechnician } = require('../controllers/reportRepair')
+const router = express.Router()
+
+router.post('/report-repair',authCheck,create)
+
+//ประวัติการแจ้งซ่อมทั้งหมด
+router.get('/report-repair/history/all',listReportRepair)
+
+//ประวัติการแจ้งซ่อม by id
+router.get('/report-repair/history/by-id/:id',readReportRepair)
+
+//ประวัติการแจ้งซ่อมทั้งหมดของ User
+router.get('/report-repair/history/by-user',authCheck,listReportRepairByUser)
+
+
+
+
+
+
+
+
+
+module.exports = router
