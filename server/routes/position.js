64beacc7628d14cd5ddfabc2633c36cc051ee80c
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-21 23:01:54 +0700

    feat: Add update route for positions and adjust role checks

--- server/routes/position.js
+++ server/routes/position.js
@@ -6,7 +6,7 @@ const { authCheck, roleCheck } = require('../middleware/authCheck')
 router.post('/position',authCheck,roleCheck(['ADMIN']),create)
 router.get('/position',authCheck,roleCheck(['ADMIN','SUPERVISOR']),listPosition)
 
-router.put('/position/:id',authCheck,roleCheck(['ADMIN']),updatePosition)
+router.patch('/position/:id',authCheck,roleCheck(['ADMIN']),updatePosition)
 router.delete('/position/:id',authCheck,roleCheck(['ADMIN']),deletePosition)
 
 module.exports = router
