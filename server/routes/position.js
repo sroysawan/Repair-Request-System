feb102a9b16e6b7cbe0f7b9ca674c20bbb23e6e1
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-31 01:30:54 +0700

    feat: Add readPosition endpoint and update listPosition permissions

--- server/routes/position.js
+++ server/routes/position.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { create, listPosition, updatePosition, deletePosition } = require('../controllers/position')
+const { create, listPosition, updatePosition, deletePosition, readPosition } = require('../controllers/position')
 const { authCheck, roleCheck } = require('../middleware/authCheck')
 
 router.post('/position',authCheck,roleCheck(['ADMIN']),create)
-router.get('/position',authCheck,roleCheck(['ADMIN','SUPERVISOR']),listPosition)
+router.get('/position',authCheck,listPosition)
 
+
+router.get('/position/:id',authCheck,readPosition)
 router.patch('/position/:id',authCheck,roleCheck(['ADMIN']),updatePosition)
 router.delete('/position/:id',authCheck,roleCheck(['ADMIN']),deletePosition)
 
