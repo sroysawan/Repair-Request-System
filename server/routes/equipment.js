6a8a22f01332475992339a41859caaca58e63bb5
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-31 01:27:54 +0700

    feat: Add statusEquipment endpoint for retrieving equipment status

--- server/routes/equipment.js
+++ server/routes/equipment.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create, listEquipment, getEquipmentByCategory, updateEquipment, readEquipment,   } = require('../controllers/equipment');
+const { create, listEquipment, getEquipmentByCategory, updateEquipment, readEquipment, statusEquipment,   } = require('../controllers/equipment');
 const { authCheck, roleCheck } = require('../middleware/authCheck');
 const router = express.Router()
 
@@ -12,5 +12,7 @@ router.get('/equipment/:id',authCheck,readEquipment)
 router.get('/equipment/category/:id',authCheck,getEquipmentByCategory)
 
 router.put('/equipment/:id',authCheck,roleCheck(['ADMIN']),updateEquipment)
+
+router.get('/status-equipment',authCheck,statusEquipment)
 
 module.exports = router
