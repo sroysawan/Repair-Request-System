e7a5ebc167b3837834dfd744e28367e9146479e6
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-02-01 23:25:26 +0700

    feat: Add readDepartment endpoint

--- server/routes/department.js
+++ server/routes/department.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { create, listDepartment, updateDepartment, deleteDepartment } = require('../controllers/department')
+const { create, listDepartment, updateDepartment, deleteDepartment, readDepartment } = require('../controllers/department')
 const { authCheck, roleCheck } = require('../middleware/authCheck')
 const router = express.Router()
 
 router.post('/department',authCheck,roleCheck(['ADMIN']),create)
+router.get('/department/:id',authCheck,readDepartment)
 router.get('/department',authCheck,roleCheck(['ADMIN','SUPERVISOR']),listDepartment)
 router.patch('/department/:id',authCheck,roleCheck(['ADMIN']),updateDepartment)
 router.delete('/department/:id',authCheck,roleCheck(['ADMIN']),deleteDepartment)
