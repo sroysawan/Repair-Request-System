4855a0b7aeec9180b56e96457cee9a11ecbf00ba
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-02-05 22:09:31 +0700

    feat: Add readCategory route and update method to PATCH for category update

--- server/routes/equipmentCategory.js.js
+++ server/routes/equipmentCategory.js.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { create, listCategory, updatedCategory, deleteCategory } = require('../controllers/equipmentCategory');
+const { create, listCategory, updatedCategory, deleteCategory, readCategory } = require('../controllers/equipmentCategory');
 const { authCheck, roleCheck } = require('../middleware/authCheck');
 const router = express.Router()
 
 router.post('/category',authCheck,roleCheck(['ADMIN']),create)
-router.get('/category',authCheck,listCategory)
 
-router.put('/category/:id',authCheck,roleCheck(['ADMIN']),updatedCategory)
+router.get('/category/:id',authCheck,readCategory)
+router.get('/category',authCheck,listCategory)
+router.patch('/category/:id',authCheck,roleCheck(['ADMIN']),updatedCategory)
 router.delete('/category/:id',authCheck,roleCheck(['ADMIN']),deleteCategory)
 
 module.exports = router
