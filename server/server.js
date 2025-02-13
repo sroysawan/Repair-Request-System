3ebeea55f95ffd47fc8e94c9014a4f6c72c91da5
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-02-13 14:36:39 +0700

    chore: Increase JSON payload limit to 10MB

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ const app = express();
 //middleware
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  limit: '10mb'
+}));
 
 readdirSync('./routes').map((r)=>{
     app.use('/api',require('./routes/'+r))
