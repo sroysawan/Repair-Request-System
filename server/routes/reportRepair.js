5af27cd57fc8c11ce92f6a0661ad7105fd257f99
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-02-18 23:54:19 +0700

    feat: Add endpoint for fetching new repair reports

--- server/routes/reportRepair.js
+++ server/routes/reportRepair.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { authCheck } = require('../middleware/authCheck')
-const { create, listReportRepair, readReportRepair, listReportRepairByUser, listReportRepairByTechnician } = require('../controllers/reportRepair')
+const { authCheck, roleCheck } = require('../middleware/authCheck')
+const { create, listReportRepair, readReportRepair, listReportRepairByUser, listReportRepairByTechnician, listReportRepairNews } = require('../controllers/reportRepair')
 const router = express.Router()
 
 router.post('/report-repair',authCheck,create)
@@ -8,11 +8,16 @@ router.post('/report-repair',authCheck,create)
 //ประวัติการแจ้งซ่อมทั้งหมด
 router.get('/report-repair/history/all',authCheck,listReportRepair)
 
+
 //ประวัติการแจ้งซ่อม by id
 router.get('/report-repair/history/by-id/:id',authCheck,readReportRepair)
 
 //ประวัติการแจ้งซ่อมทั้งหมดของ User
 router.get('/report-repair/history/by-user',authCheck,listReportRepairByUser)
+
+
+//supervisor technician
+router.get('/report-repair/news',authCheck,roleCheck(['ADMIN','SUPERVISOR','TECHNICIAN']),listReportRepairNews)
 
 
 
