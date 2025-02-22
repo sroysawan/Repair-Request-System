1881383b49f9ee444e8f04344570605201bc3224
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-02-23 00:46:24 +0700

    feat: Add image and assignment schemas for user and report validation

--- client/src/utils/schema.js
+++ client/src/utils/schema.js
@@ -35,6 +35,7 @@ export const registerSchema = z.object({
   departmentId: z.coerce.number(),
   positionId: z.coerce.number(),
   role: z.string().nonempty("กรุณาเลือกสิทธิ์"),
+  pictureId: z.any(),
 });
 
 export const positionSchema = z.object({
@@ -73,6 +74,7 @@ export const equipmentSchema = z.object({
     .nonempty("กรุณากรอกสถานที่")
     .min(2, "สถานที่ต้องมากกว่า 2 ตัวอักษร"),
   statusEquipment: z.string().nonempty("กรุณาเลือกสถานะของอุปกรณ์"),
+  images: z.any(),
 });
 
 export const reportSchema = z.object({
@@ -89,4 +91,53 @@ export const reportSchema = z.object({
     .string()
     .nonempty("กรุณากรอกสถานที่")
     .min(2, "สถานที่ต้องมากกว่า 2 ตัวอักษร"),
+  images: z.any(),
+});
+
+export const assignReportSchemaBySupervisor = z.object({
+  technicianId: z.coerce.number(),
+});
+
+export const successAssignmentSchemaByTechnician = z.object({
+  solution: z
+    .string()
+    .nonempty("กรุณากรอกคำอธิบายการแก้ไข")
+    .min(2, "คำอธิบายการแก้ไขต้องมากกว่า 2 ตัวอักษร"),
+  status: z.string().nonempty("กรุณาเลือกสถานะการส่งงาน"),
+});
+
+export const passwordSchema = z.object({
+  password: z
+  .string()
+  .nonempty("กรุณากรอกรหัสผ่าน") // ห้ามเว้นว่าง
+  .min(8, "รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร"),
+})
+
+
+export const updateUserSchema = z.object({
+  firstName: z
+    .string()
+    .nonempty("กรุณากรอกชื่อ")
+    .min(2, "ชื่อต้องมากกว่า 2 ตัวอักษร"),
+  lastName: z
+    .string()
+    .nonempty("กรุณากรอกนามสกุล")
+    .min(2, "นามสกุลต้องมากกว่า 2 ตัวอักษร"),
+  email: z.string().nonempty("กรุณากรอกอีเมล").email("รูปแบบอีเมลไม่ถูกต้อง"),
+  tel: z
+    .string()
+    .nonempty("กรุณากรอกเบอร์โทร")
+    .regex(/^[0-9+()-\s]+$/, "กรุณากรอกเบอร์โทรให้ถูกต้อง"),
+  userName: z
+    .string()
+    .nonempty("กรุณากรอก Username") // ห้ามเว้นว่าง
+    .min(3, "Username ต้องมีความยาวอย่างน้อย 3 ตัวอักษร")
+    .max(20, "Username ต้องไม่เกิน 20 ตัวอักษร")
+    .regex(
+      /^[a-zA-Z0-9_.]+$/,
+      "Username อนุญาตเฉพาะตัวอักษร, ตัวเลข, _ และ . เท่านั้น"
+    ),
+  departmentId: z.coerce.number(),
+  positionId: z.coerce.number(),
+  pictureId: z.any(),
 });
