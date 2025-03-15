76ee0c0c0f1edb1def6177f7cda90f6ad0791346
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-03-15 13:20:08 +0700

    feat: add endpoints for listing and completing assignments

--- server/routes/repairHistory.js
+++ server/routes/repairHistory.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { completeAssignment } = require('../controllers/repairHistory');
+const { completeAssignment, listRepairHistory, listRepairHistoryByTechnician, completeAssignmentBySupervisor, readRepairHistory } = require('../controllers/repairHistory');
 const { authCheck } = require('../middleware/authCheck');
 const router = express.Router();
 
 router.put('/complete-assignment',authCheck, completeAssignment);
+router.put('/complete-assignment/by-supervisor',authCheck, completeAssignmentBySupervisor);
+router.get('/repair-history',authCheck, listRepairHistory);
 
+router.get('/repair-history/by-technician',authCheck,listRepairHistoryByTechnician)
+router.get('/repair-history/:id',authCheck,readRepairHistory)
 
 module.exports = router
